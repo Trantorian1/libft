3d6a997b74b36287fc65f134148c937390adf1b2
emcnab <eliot.code@proton.me>
2022-11-29 14:33:35 +0100

    fix: normed all converter-related functions

--- data_structures/converter/ft_converter.c
+++ data_structures/converter/ft_converter.c
@@ -6,7 +6,7 @@
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 14:35:19 by emcnab            #+#    #+#             */
-/*   Updated: 2022/11/28 16:07:52 by emcnab           ###   ########.fr       */
+/*   Updated: 2022/11/29 14:32:51 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ t_conv	*ft_converter(void)
 {
 	t_conv	*conv;
 
-	if (!(conv = malloc(sizeof(*conv))))
+	conv = malloc(sizeof(*conv));
+	if (!conv)
 	{
 		return (NULL);
 	}
