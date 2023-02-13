006e93d979052c6312a6826a5374bc29cd9ee1a1
emcnab <eliot.code@proton.me>
2023-02-13 10:40:32 +0100

    fix(strcmp): moved strmp to a sensible signed implementation

--- src/str/ft_strcmp.c
+++ src/str/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 09:29:22 by emcnab            #+#    #+#             */
-/*   Updated: 2023/01/12 14:39:30 by emcnab           ###   ########.fr       */
+/*   Updated: 2023/02/13 10:39:25 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@
  */
 int	ft_strcmp(const char *str_a, const char *str_b)
 {
-	unsigned char	char_dif;
-	unsigned char	char_a;
-	unsigned char	char_b;
+	char	char_dif;
+	char	char_a;
+	char	char_b;
 
 	while (true)
 	{
-		char_a = *((const unsigned char *)str_a);
-		char_b = *((const unsigned char *)str_b);
+		char_a = *(str_a);
+		char_b = *(str_b);
 		char_dif = char_a - char_b;
 		if (!*str_a++ || !*str_b++ || char_dif)
 			return (char_dif);
