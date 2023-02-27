f6b751bb19b15b7a5dd9cc2a998186adc968b103
emcnab <eliot.code@proton.me>
2023-02-27 16:42:16 +0100

    docs(ft_malloc): added doccumentation to ft_malloc.c

--- src/mem/ft_malloc.c
+++ src/mem/ft_malloc.c
@@ -6,7 +6,7 @@
 /*   By: emcnab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 14:14:59 by emcnab            #+#    #+#             */
-/*   Updated: 2023/02/27 15:58:13 by emcnab           ###   ########.fr       */
+/*   Updated: 2023/02/27 16:41:17 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,20 @@
 
 static t_s_linkmem	*g_last = NULL;
 
+// TODO: implement other strategies for handling allocation errors.
+/**
+ * @brief Allocates space in memory for a pointer.
+ *
+ * Allocations are stored as part of a linked list, where the pointer to a node
+ * shares memory with the allocated pointer. This way, a single malloc / free is
+ * necesary to allocate / free the node and its associated memory.
+ *
+ * @param size (size_t): The number of bytes to allocate.
+ * 
+ * @return (void *): Pointer to allocated space in memory.
+ * @warning Frees all previous allocations and exits program in case of
+ * allocation failure. This guarantees that the function can never return NULL.
+ */
 void	*ft_malloc(size_t size)
 {
 	t_s_linkmem			*node;
@@ -46,6 +60,19 @@ void	*ft_malloc(size_t size)
 	return (data);
 }
 
+/**
+ * @brief Reallocates space in memory for a pointer, copying over its initial
+ * data.
+ *
+ * Note that if the amount of bytes to resize the pointer to is smaller that its
+ * current size, then not all bytes will be copied. While this might work for
+ * arrays, it should not be used for other data types such as structures.
+ *
+ * @param ptr (void *): Pointer to reallocate.
+ * @param size_new (size_t): Amount of bytes to resize the pointer to.
+ *
+ * @return (void *): Resized pointer.
+ */
 void	*ft_realloc(void *ptr, size_t size_new)
 {
 	t_s_linkmem	*node_old;
@@ -62,6 +89,19 @@ void	*ft_realloc(void *ptr, size_t size_new)
 	return (data_new);
 }
 
+/**
+ * @brief Frees up allocated space in memory.
+ *
+ * In freeing the pointer, also frees the associates node and updates the
+ * allocation list.
+ *
+ * @param ptr (void *): Pointer to free.
+ *
+ * @return (void): NULL
+ *
+ * @warning This function only accepts pointers that have been allocated using
+ * ft_malloc.
+ */
 void	*ft_free(void *ptr)
 {
 	t_s_linkmem	*node;
@@ -79,6 +119,11 @@ void	*ft_free(void *ptr)
 	return (NULL);
 }
 
+/**
+ * @brief Frees all memory allocated with ft_malloc.
+ *
+ * @return (void *): NULL
+ */
 void	*ft_free_all(void)
 {
 	t_s_linkmem	*node_curr;
